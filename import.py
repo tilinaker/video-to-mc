e2e8d03d4749dcadeb92face5d7afca51202c22d
BuildTools <unconfigured@null.spigotmc.org>
2018-12-28 19:49:49 +0900

    update import.py

--- import.py
+++ import.py
@@ -5,19 +5,21 @@ import numpy as np
 
 
 def load_mix(filename):
-    img = cv2.imread(filename, 1)
+    imgrgb = cv2.imread(filename, 1)
     
-    if type(img) == type(None):
+    
+    if type(imgrgb) == type(None):
         return 0
 
+    img = cv2.cvtColor(imgrgb, cv2.COLOR_BGR2HSV)
     result = np.zeros(3)
     for i in range(16):
         for j in range(16):
-            result = result + hsv(img[i][j][0], img[i][j][1], img[i][j][2])
+            result = result + img[i][j]
 
     
 
-    return (rgb(result[2], result[1], result[0]))
+    return (result[0], result[1], result[2])
 
 def main():
     result = []
@@ -45,46 +47,6 @@ def main():
     print(len(result))
     print(result)
 
-def hsv(B, G, R):
-    r, g, b = R/255, G/255, B/255
-    mx = max(r, g, b)
-    mn = min(r, g, b)
-    df = mx-mn
-    if mx == mn:
-        h = 0
-    elif mx == r:
-        h = (60 * ((g-b)/df) + 360) % 360
-    elif mx == g:
-        h = (60 * ((b-r)/df) + 120) % 360
-    elif mx == b:
-        h = (60 * ((r-g)/df) + 240) % 360
-    if mx == 0:
-        s = 0
-    else:
-        s = df/mx
-    v = mx
-    return h, s, v
-
-def rgb(h, s, v):
-    h = float(h)
-    s = float(s)
-    v = float(v)
-    h60 = h / 60.0
-    h60f = math.floor(h60)
-    hi = int(h60f) % 6
-    f = h60 - h60f
-    p = v * (1 - s)
-    q = v * (1 - f * s)
-    t = v * (1 - (1 - f) * s)
-    r, g, b = 0, 0, 0
-    if hi == 0: r, g, b = v, t, p
-    elif hi == 1: r, g, b = q, v, p
-    elif hi == 2: r, g, b = p, v, t
-    elif hi == 3: r, g, b = p, q, v
-    elif hi == 4: r, g, b = t, p, v
-    elif hi == 5: r, g, b = v, p, q
-    r, g, b = int(r * 255), int(g * 255), int(b * 255)
-    return r, g, b
 
 main()
 
